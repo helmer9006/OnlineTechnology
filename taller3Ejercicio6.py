8b4b9545245cd56ec2b9e7f41af4180ae6691e5d
helmer9006 <59346451+helmer9006@users.noreply.github.com>
2020-11-21 11:43:15 -0500

    funcionalidad crear nuevo producto

--- taller3Ejercicio6.py
+++ taller3Ejercicio6.py
@@ -1,50 +1,14 @@
 
 
-productos = {
-    "CAMARAS": {
-        "camara Domo Plastico 720p": {
-            "codigo": 'cam1',
-            "precioCompra": 80000,
-            "precioVenta": 104000
-        },
-        "camara Domo Metalico 720p": {
-            "codigo": 'cam2',
-            "precioCompra": 80000,
-            "precioVenta": 104000
-        },
-        "camara Domo Metalico 1080p": {
-            "codigo": 'cam3',
-            "precioCompra": 80000,
-            "precioVenta": 104000
-        },
-        "camara Domo Plastico 1080p": {
-            "codigo": 'cam4',
-            "precioCompra": 105000,
-            "precioVenta": 129000
-        },
-        "camara Domo Plastico 1080p": {
-            "codigo": 'cam5',
-            "precioCompra": 95000,
-            "precioVenta": 119000
-        },
+# region CREAR PRODUCTO
+productos = [] # nombre, precioCompra, precioventa
+def crearProducto():
+    nombre = input('Por favor, ingresar nombre del producto: ')
+    precioCompra = float(input('Por favor, ingresar precio de compra del producto: '))
+    precioVenta = float(input('Por favor, ingresar precio de venta para el producto: '))
+    nuevoProducto = {"nombre":nombre, "precioCompra": precioCompra, "precioVenta":precioVenta} 
+    productos.append(nuevoProducto)
 
-    },
-    "DVR": {
-        "dvr 8 Canales": {
-            "codigo": 'dvr1',
-            "precioCompra": 145000,
-            "precioVenta": 188500
-        },
-        "dvr 8 Canales": {
-            "codigo": 'dvr1',
-            "precioCompra": 185000,
-            "precioVenta": 240500
-        },
-        "dvr 32 Canales": {
-            "codigo": 'dvr1',
-            "precioCompra": 215000,
-            "precioVenta": 279500
-        }
-    }
-}
-print(productos)
+
+
+crearProducto()
