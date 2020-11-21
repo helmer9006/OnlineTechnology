086fe220c1b49a7e4b84b96ef62b153d717fdba6
helmer9006 <59346451+helmer9006@users.noreply.github.com>
2020-11-21 10:01:49 -0500

    Inicio proyecto

---
+++ taller3Ejercicio6.py
@@ -0,0 +1,50 @@
+
+
+productos = {
+    "CAMARAS": {
+        "camara Domo Plastico 720p": {
+            "codigo": 'cam1',
+            "precioCompra": 80000,
+            "precioVenta": 104000
+        },
+        "camara Domo Metalico 720p": {
+            "codigo": 'cam2',
+            "precioCompra": 80000,
+            "precioVenta": 104000
+        },
+        "camara Domo Metalico 1080p": {
+            "codigo": 'cam3',
+            "precioCompra": 80000,
+            "precioVenta": 104000
+        },
+        "camara Domo Plastico 1080p": {
+            "codigo": 'cam4',
+            "precioCompra": 105000,
+            "precioVenta": 129000
+        },
+        "camara Domo Plastico 1080p": {
+            "codigo": 'cam5',
+            "precioCompra": 95000,
+            "precioVenta": 119000
+        },
+
+    },
+    "DVR": {
+        "dvr 8 Canales": {
+            "codigo": 'dvr1',
+            "precioCompra": 145000,
+            "precioVenta": 188500
+        },
+        "dvr 8 Canales": {
+            "codigo": 'dvr1',
+            "precioCompra": 185000,
+            "precioVenta": 240500
+        },
+        "dvr 32 Canales": {
+            "codigo": 'dvr1',
+            "precioCompra": 215000,
+            "precioVenta": 279500
+        }
+    }
+}
+print(productos)
